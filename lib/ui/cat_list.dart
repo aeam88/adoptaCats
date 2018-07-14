0f1abf93600c7086473fc0e9f62367ad222e7b66
Alejandro Alvarez <a.alvarezmontes@gmail.com>
2018-07-14 09:04:54 -0400

    Lista de gatos y despliegue

--- lib/ui/cat_list.dart
+++ lib/ui/cat_list.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:adopta_cats/models/cat.dart';
+import 'package:adopta_cats/services/api.dart';
 import 'package:flutter/material.dart';
 
 class CatList extends StatefulWidget {
@@ -6,10 +10,25 @@ class CatList extends StatefulWidget {
 }
 
 class _CatListState extends State<CatList> {
+  List<Cat> _cats = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCats();
+  }
+
+  _loadCats() async {
+    String fileData = await DefaultAssetBundle.of(context).loadString("assets/cats.json");
+    setState(() {
+          _cats = CatApi.allCatsFromJson(fileData);
+    });
+
+  }
   
   Widget _getAppTitleWidget() {
     return new Text(
-      'Cats',
+      'Gatos',
       style: new TextStyle(
         color: Colors.white,
         fontWeight: FontWeight.bold,
@@ -18,11 +37,78 @@ class _CatListState extends State<CatList> {
     );
   }
 
+  Widget _buildBody() {
+    return new Container(
+      margin: const EdgeInsets.fromLTRB(
+        8.0,
+        56.0,
+        8.0,
+        0.0
+      ),
+      child: new Column(
+        children: <Widget>[
+          _getAppTitleWidget(),
+          _getListViewWidget()
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCatItem(BuildContext context, int index) {
+    Cat cat = _cats[index];
+
+    return new Container(
+      margin: const EdgeInsets.only(top: 5.0),
+      child: new Card(
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            new ListTile(
+              //onTap: //HACER
+              leading: new Hero(
+                tag: index,
+                child: new CircleAvatar(
+                  backgroundImage: new NetworkImage(cat.avatarUrl),
+                ),
+              ),
+              title: new Text(
+                cat.name,
+                style: new TextStyle(fontWeight: FontWeight.bold, color: Colors.black54),
+              ),
+              subtitle: new Text(cat.description),
+              isThreeLine: true,
+              dense: false,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Null> refresh() {
+    _loadCats();
+    return new Future<Null>.value();
+  }
+
+  Widget _getListViewWidget() {
+    return new Flexible(
+      child: new RefreshIndicator(
+        onRefresh: refresh,
+        child: new ListView.builder(
+          physics: const AlwaysScrollableScrollPhysics(),
+          itemCount: _cats.length,
+          itemBuilder: _buildCatItem,
+        ),
+      ),
+
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Colors.lightGreen,
-      body: _getAppTitleWidget(),
+      body: _buildBody(),
     );
   }
 }
